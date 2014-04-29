aae9f72ea797a9a49bcf6fce4ae9a8889a41b235
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-04-29 14:30:44 -0400

    initial commit of application

---
+++ config/routes.rb
@@ -0,0 +1,42 @@
+Rails.application.routes.draw do
+  
+  namespace :static do 
+    get 'home',     to: 'home#index'
+    get 'about',    to: 'home#about'
+    get 'support',  to: 'home#help'
+  end
+
+  namespace :api, default: :json do 
+    namespace :v1 do
+
+      get :search,        to: 'places#search'
+
+      get 'guides/:slug',  to:  'guides#show'
+
+      resources :users do
+
+        get :places,      to: 'users/places#index'
+        get :hotspots,    to: 'users/hotspots#index'
+        get :guides,      to: 'users/guides#index'
+
+        member do 
+          get :home, to:  'users#dashboard'
+        end
+      end
+
+      # 
+      resource :favorites do 
+        post :places,     to: 'favorites/places#create'
+        post :hotspots,   to: 'favorites/hotspots#create'
+        post :guides,     to: 'favorites/guides#create'
+
+        member do 
+          get :home, to:  'users#dashboard'
+        end
+
+      end
+
+
+    end
+  end
+end
